b0ef7f32b4ab5a8395b4f7da6a26485334035f99
manu154c <manucherupoyliath@gmail.com>
2020-06-14 05:20:41 +0530

    first local commit

---
+++ polls/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+import datetime
+from django.utils import timezone
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        starting_time_boundary = now - datetime.timedelta(days=1)
+        #ending_time_boundary = now + datetime.timedelta(days=1) Logic error -> Future 1 Day
+        ending_time_boundary = now
+        return ending_time_boundary >= self.pub_date >= starting_time_boundary
+    
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
+
+class Register(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
